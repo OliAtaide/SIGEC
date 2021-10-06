54bb107876cd89aeb9d68e9219577d6e1db76f58
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2021-10-06 15:26:05 -0300

    Implementacao parcial do filtro de casos

--- src/views/Busca.vue
+++ src/views/Busca.vue
@@ -7,7 +7,7 @@
       <v-col>
         <v-row>
           <v-icon>mdi-home-outline</v-icon>
-          <router-link  to="dashboard" class="mr-1" href="">Início</router-link>
+          <router-link to="dashboard" class="mr-1" href="">Início</router-link>
           <p class="ma-0">/ Gestão de caso</p>
         </v-row>
         <v-row>
@@ -19,6 +19,7 @@
             <v-row class="ma-10 d-flex">
               <v-subheader>Pesquisar pelo nome ou CPF</v-subheader>
               <v-text-field
+                class="pesquisa"
                 v-model="pesquisa"
                 dense
                 outlined
@@ -37,10 +38,150 @@
                 <v-icon left> mdi-magnify</v-icon>
                 Buscar
               </v-btn>
-              <v-btn rounded outlined color="#0C109C" class="text-none">
+              <v-btn @click="filtro.visivel = !filtro.visivel" rounded outlined color="#0C109C" class="text-none">
                 <v-icon left> mdi-filter-outline</v-icon>
                 Filtrar
               </v-btn>
+            </v-row>
+            <v-row v-if="filtro.visivel" class="ma-10">
+              <v-card outlined>
+                <v-card-title>Filtros</v-card-title>
+                <v-card-text
+                  >Preencha pelo menos um dos campos de pesquisa
+                  abaixo:</v-card-text
+                >
+                <v-col>
+                  <v-row class="pb-5">
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Data de nascimento</v-subheader>
+                      <v-text-field
+                        type="date"
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        v-model="filtro.data_de_nascimento"
+                      >
+                      </v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Sexo</v-subheader>
+                      <v-select
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        :items="['', 'Masculino', 'Feminino', 'Não informado']"
+                        v-model="filtro.sexo"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Nome da mãe</v-subheader>
+                      <v-text-field
+                        placeholder="Ex.: Maria da Silva"
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Bairro</v-subheader>
+                      <v-text-field
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        v-model="filtro.bairro"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Escolaridade</v-subheader>
+                      <v-select
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        :items="['', 'fundamental', 'medio', 'superior']"
+                        v-model="filtro.escolaridade"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Raça</v-subheader>
+                      <v-text-field
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        v-model="filtro.raca"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Status</v-subheader>
+                      <v-select
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        :items="[
+                          '',
+                          'Diagnosticado',
+                          'Descartado',
+                          'Em tratamento',
+                          'Encerrado',
+                        ]"
+                        v-model="filtro.status"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Data de abertura</v-subheader>
+                      <v-text-field
+                        type="date"
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        v-model="filtro.data_abertura"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" class="py-0">
+                      <v-subheader>Data de encerramento</v-subheader>
+                      <v-text-field
+                        type="date"
+                        rounded
+                        outlined
+                        dense
+                        hide-details
+                        v-model="filtro.data_encerramento"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col
+                      ><v-btn
+                        @click="limparFiltro()"
+                        dark
+                        rounded
+                        color="#FCA311"
+                        class="text-none mr-5"
+                      >
+                        <v-icon left>mdi-eraser</v-icon>
+                        Limpar
+                      </v-btn>
+                      <v-btn
+                        @click="buscaCasos()"
+                        dark
+                        rounded
+                        color="#0C109C"
+                        class="text-none"
+                      >
+                        <v-icon left>mdi-magnify</v-icon>
+                        Buscar
+                      </v-btn></v-col
+                    >
+                  </v-row>
+                </v-col>
+              </v-card>
             </v-row>
             <v-row v-if="casos.length == 0" class="ma-10">
               <v-card
@@ -128,55 +269,76 @@ export default {
     return {
       casos: [],
       pesquisa: "",
+      filtro: {
+        visivel: false,
+        data_de_nascimento: "",
+        bairro: "",
+        sexo: "",
+        escolaridade: "",
+        raca: "",
+        status: "",
+        data_abertura: "",
+        data_encerramento: "",
+      },
     };
   },
 
   methods: {
     fetchCasos() {
       axios
-        .get("https://gestaodecasos-edital043.vercel.app/api/casos")
+        .get("/casos")
         .then((response) => (this.casos = response.data.data))
         .catch((error) => console.log(error));
     },
 
     buscaCasos() {
-      if (this.pesquisa != "") {
-        axios
-          .get("https://gestaodecasos-edital043.vercel.app/api/casos")
-          .then((response) => {
-            var casos = response.data.data;
-            this.casos = casos.filter((caso) => this.filtrarCasos(caso));
-          })
-          .catch((error) => console.log(error));
-        // this.casos = this.casos.filter((caso) => this.filtrarCasos(caso));
-      } else {
-        this.fetchCasos();
-      }
-      console.log(this.casos);
+      axios
+        .get("/casos")
+        .then((response) => {
+          var casos = response.data.data;
+          this.casos = casos.filter((caso) => this.filtrarCasos(caso));
+        })
+        .catch((error) => console.log(error));
+      // this.casos = this.casos.filter((caso) => this.filtrarCasos(caso));
     },
 
     getIdade(d) {
       const dia = d.slice(0, 2);
       const mes = d.slice(3, 5);
       const ano = d.slice(6, 10);
-      // console.log(ano + "/" + mes + "/" + dia);
       const dn = new Date(ano, mes, dia);
-      // var dian = d.split(0,1);
-      // console.log(dian);
       const agora = new Date();
       const date = Math.abs(dn - agora);
-      // const dia = date.getDate();
-      // const month = date.getMonth();
-      // const year = date.getFullYear();
-      // return dia + "/" + month + "/" + year;
       return parseInt(date / 31536000000);
     },
 
     filtrarCasos(caso) {
-      if (caso.nome.toLowerCase().includes(this.pesquisa)) {
+      var casoNome =
+        caso.nome.toLowerCase().includes(this.pesquisa) || this.pesquisa == "";
+      var casoSexo = caso.sexo == this.filtro.sexo || this.filtro.sexo == "";
+      var casoEsco =
+        caso.escolaridade == this.filtro.escolaridade ||
+        this.filtro.escolaridade == "";
+      var casoRaca = caso.raca == this.filtro.raca || this.filtro.raca == "";
+      var casoStat =
+        caso.status == this.filtro.status || this.filtro.status == "";
+
+      if (casoNome && casoSexo && casoEsco && casoRaca && casoStat) {
         return caso;
       }
     },
+
+    limparFiltro(){
+      this.filtro.data_de_nascimento = '';
+      this.filtro.bairro = '';
+      this.filtro.sexo = '';
+      this.filtro.escolaridade = '';
+      this.filtro.raca = '';
+      this.filtro.status = '';
+      this.filtro.data_abertura = '';
+      this.filtro.data_encerramento = '';
+      console.log('ok');
+    }
   },
 };
 </script>
@@ -186,13 +348,16 @@ export default {
   padding-left: 18%;
   padding-right: 18%;
 }
-.sheet{
+.sheet {
   background-color: inherit !important;
 }
 .resultado {
   display: flex !important;
 }
-.v-text-field{
+.pesquisa {
   width: 10% !important;
+}
+.filtros{
+
 }
 </style>
